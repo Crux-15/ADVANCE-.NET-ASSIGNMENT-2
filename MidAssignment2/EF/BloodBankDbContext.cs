b70aac16b74adf6144204094e71a5c4309cfe18f
SADMAN SAKIB <23-50636-1@student.aiub.edu>
2026-07-25 03:07:54 +0600

    Add project files.

---
+++ MidAssignment2/EF/BloodBankDbContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using MidAssignment2.EF.Tables;
+
+namespace MidAssignment2.EF;
+
+public partial class BloodBankDbContext : DbContext
+{
+    public BloodBankDbContext()
+    {
+    }
+
+    public BloodBankDbContext(DbContextOptions<BloodBankDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Donation> Donations { get; set; }
+
+    public virtual DbSet<Donor> Donors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=DbConn");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Donation>(entity =>
+        {
+            entity.ToTable("Donation");
+
+            entity.Property(e => e.DonationId).ValueGeneratedNever();
+            entity.Property(e => e.CampName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Donor).WithMany(p => p.Donations)
+                .HasForeignKey(d => d.DonorId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Donation_Donor");
+        });
+
+        modelBuilder.Entity<Donor>(entity =>
+        {
+            entity.ToTable("Donor");
+
+            entity.Property(e => e.BloodGroup)
+                .HasMaxLength(3)
+                .IsUnicode(false);
+            entity.Property(e => e.City)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.ContactNo)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.FullName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
