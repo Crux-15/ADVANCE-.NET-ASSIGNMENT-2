7944b10378e92d7503801941c186df4f6751d574
SADMAN SAKIB <23-50636-1@student.aiub.edu>
2026-07-25 03:30:30 +0600

    Dependency injection Done & DonorController Added

---
+++ MidAssignment2/Controllers/DonorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MidAssignment2.EF;
+
+namespace MidAssignment2.Controllers
+{
+    public class DonorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MidAssignment2/Program.cs
+++ MidAssignment2/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MidAssignment2.EF;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<BloodBankDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConn"));
+});
 
 var app = builder.Build();
 
