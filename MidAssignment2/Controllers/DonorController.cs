c27d32ab1589bbcac6113060826fa36915175277
SADMAN SAKIB <23-50636-1@student.aiub.edu>
2026-07-25 18:22:55 +0600

    Ceate Donor & DonorList Done

--- MidAssignment2/Controllers/DonorController.cs
+++ MidAssignment2/Controllers/DonorController.cs
@@ -1,13 +1,92 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MidAssignment2.EF;
+using MidAssignment2.EF.Tables;
+using MidAssignment2.Models;
 
 namespace MidAssignment2.Controllers
 {
     public class DonorController : Controller
     {
-        public IActionResult Index()
+        BloodBankDbContext db;
+
+        public DonorController(BloodBankDbContext db)
         {
-            return View();
+            this.db = db;
         }
+        public IActionResult DonorList(string BloodGroup)
+        {
+            if (string.IsNullOrEmpty(BloodGroup) || BloodGroup == "All")
+            {
+                var data = (from d in db.Donors
+                            join d2 in db.Donations
+                            on d.DonorId equals d2.DonorId into donations
+                            orderby d.LastDonationDate descending
+                            select new
+                            {
+                                d.DonorId,
+                                d.FullName,
+                                d.BloodGroup,
+                                d.ContactNo,
+                                d.City,
+                                d.LastDonationDate,
+                                DonationCount = donations.Count()
+
+                            }
+                            ).ToList();
+                return View(data);
+            }
+            else
+            {
+                var data = (from d in db.Donors
+                            join d2 in db.Donations
+                            on d.DonorId equals d2.DonorId into donations
+                            where d.BloodGroup == BloodGroup
+                            orderby d.LastDonationDate descending
+                            select new
+                            {
+                                d.DonorId,
+                                d.FullName,
+                                d.BloodGroup,
+                                d.ContactNo,
+                                d.City,
+                                d.LastDonationDate,
+                                DonationCount = donations.Count()
+                            }
+                            ).ToList();
+                return View(data);
+            }
+
+            
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new DonorsModel());
+        }
+
+        [HttpPost]
+        public IActionResult Create(DonorsModel d)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(d);
+            } 
+
+            Donor dt = new Donor();
+            {
+                dt.FullName = d.FullName;
+                dt.BloodGroup = d.BloodGroup;
+                dt.ContactNo = d.ContactNumber;
+                dt.City = d.City;
+                dt.LastDonationDate = DateOnly.FromDateTime(d.LastDonatedDate);
+            };
+
+            db.Donors.Add(dt);
+            db.SaveChanges();
+            return RedirectToAction("DonorList");
+
+        }
+
     }
 }
---
+++ MidAssignment2/Models/DonorsModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MidAssignment2.Models
+{
+    public class DonorsModel
+    {
+        public int DonorId { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Blood Group is required")]
+        [RegularExpression("^(A|B|AB|O)[+-]$", ErrorMessage = "Invalid Blood Group format. Use A+, A-, B+, B-, AB+, AB-, O+, or O-.")]
+        public string BloodGroup { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Contact Number is required")]
+        public string ContactNumber { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "City is required")]
+        public string City { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last Donated Date is required")]
+        public DateTime LastDonatedDate { get; set; }
+
+    }
+}
